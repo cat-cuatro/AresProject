0e1fed9cb6b13e43195a0f1618ed7923f0779e33
John Lorenz IV <john.darc.4@gmail.com>
2020-11-12 19:33:38 -0800

    expanded background picture dimensions, added scrolling effect

--- background.py
+++ background.py
@@ -2,74 +2,51 @@ import pygame
 from math import sqrt
 class space:
     def __init__(self, screen):
-        self.bgQuadrant1 = pygame.image.load('backgrounds/rough_background.png')
-        self.bgQuadrant2 = pygame.image.load('backgrounds/rough_background.png')
-        self.bgQuadrant3 = pygame.image.load('backgrounds/rough_background.png')
-        self.bgQuadrant4 = pygame.image.load('backgrounds/rough_background.png')
+        self.centerDiagonalPanel = pygame.image.load('backgrounds/rough_background_square.png')
+        self.centerDiagonalPanel2 = pygame.image.load('backgrounds/rough_background_square.png')
+        self.upperDiagonalPanel = pygame.image.load('backgrounds/rough_background_square.png')
+        self.upperDiagonalPanel2= pygame.image.load('backgrounds/rough_background_square.png')
+        self.lowerDiagonalPanel = pygame.image.load('backgrounds/rough_background_square.png')
+        self.lowerDiagonalPanel2= pygame.image.load('backgrounds/rough_background_square.png')
         self.backgroundPositions = {
-            self.bgQuadrant1: [0, 0, (0, 0)],
-            self.bgQuadrant2: [0, -1080, (0, -1080)],
-            self.bgQuadrant3: [1920, -1080, (1920, -1080)],
-            self.bgQuadrant4: [1920, 0, (1920, 0)],
+            self.centerDiagonalPanel: [0, 0, (0, 0)],
+            self.upperDiagonalPanel: [0, -1920, (0, -1920)],
+            self.upperDiagonalPanel2: [1920, -3840, (1920, -3840)],
+            self.centerDiagonalPanel2: [1920, -1920, (1920, -1920)],
+            self.lowerDiagonalPanel: [1920, 0, (1920, 0)],
+            self.lowerDiagonalPanel2: [3840, -1920, (3840, -1920)],
         }
         self.screen = screen
-        self.firstPanelEndpoint = [1920, 0]
-        self.secondPanelEndpoint = [3840, 0]
-    
-    def debugLines(self, dt):
-        self.firstPanelEndpoint[0] -= 1 * dt/2
-        self.firstPanelEndpoint[1] += 1 * dt/2
-        self.secondPanelEndpoint[0] -= 1 * dt/2
-        self.firstPanelEndpoint[1] += 1 * dt/2
-
-        if self.firstPanelEndpoint[0] <= 0:
-            self.firstPanelEndpoint = [1920, 0]
-        if self.secondPanelEndpoint[0] <= 0:
-            self.secondPanelEndpoint = [3840, 0]
-
-        coords = self.firstPanelEndpoint
-        firstBoundRect = pygame.Rect(coords[0], coords[1], 20, 1080)
-        coords = self.secondPanelEndpoint
-        secondBoundRect = pygame.Rect(coords[0], coords[1], 20, 1080)
-
-        pygame.draw.rect(self.screen, (255, 0, 0), firstBoundRect)
-        pygame.draw.rect(self.screen, (0, 255, 255), secondBoundRect)
 
     def drawSpace(self, dt):
         self.moveBackground(dt)
-        '''
+
         for background in self.backgroundPositions:
-            #print(self.backgroundPositions[background])
             self.screen.blit(background, (self.backgroundPositions[background][0], self.backgroundPositions[background][1]))
-        '''
-        bg = self.bgQuadrant1
-        self.screen.blit(bg, (self.backgroundPositions[bg][0], self.backgroundPositions[bg][1]))
-        bg = self.bgQuadrant2
-        self.screen.blit(bg, (self.backgroundPositions[bg][0], self.backgroundPositions[bg][1]))
-        bg = self.bgQuadrant3
-        self.screen.blit(bg, (self.backgroundPositions[bg][0], self.backgroundPositions[bg][1]))
-        bg = self.bgQuadrant4
-        self.screen.blit(bg, (self.backgroundPositions[bg][0], self.backgroundPositions[bg][1]))
-        
-        #self.debugLines(dt)
     
     def moveBackground(self, dt):
-        #print(self.backgroundPositions[self.bgQuadrant1])
         for background in self.backgroundPositions:
-            xOrigin = self.backgroundPositions[background][2][0]
-            yOrigin = self.backgroundPositions[background][2][1]
+            self.backgroundPositions[background][0] -= int(1 * (dt/6))
+            self.backgroundPositions[background][1] += int(1 * (dt/6))
+            self.checkPanelPosition(background)
+    
+    def checkPanelPosition(self, panel):
+        xPos = self.backgroundPositions[panel][0]
+        yPos = self.backgroundPositions[panel][1]
+        if panel == self.upperDiagonalPanel or panel == self.upperDiagonalPanel2:
+            # check upper positions (-1920, 0) ** hard coded 1920x1920 dimensions
+            if xPos <= -1920 and yPos >= 0:
+                self.rebufferPanel(panel, 1920, -3840)
+        elif panel == self.centerDiagonalPanel or panel == self.centerDiagonalPanel2:
+            # check center positions (-1920, 1080)
+            if xPos <= -1920 and yPos >= 1920:
+                self.rebufferPanel(panel, 1920, -1920)
+        elif panel == self.lowerDiagonalPanel or panel == self.lowerDiagonalPanel2:
+            # check lower positions (0, 1080)
+            if xPos <= 0 and yPos >= 1920:
+                self.rebufferPanel(panel, 3840, -1920)
 
-
-            self.backgroundPositions[background][0] -= int(1 * (dt/4))
-            self.backgroundPositions[background][1] += int(1 * (dt/4))
-            xPos = self.backgroundPositions[background][0]
-            yPos = self.backgroundPositions[background][1]
-
-            xDiff = (xPos - xOrigin)**2
-            yDiff = (yPos - yOrigin)**2
-            distance = sqrt(xDiff+yDiff)
-            
-            #print(xDiff, yDiff)
-            if distance >= 2202:
-                self.backgroundPositions[background][0] = xOrigin
-                self.backgroundPositions[background][1] = yOrigin
+    
+    def rebufferPanel(self, panel, newX, newY):
+        self.backgroundPositions[panel][0] = newX
+        self.backgroundPositions[panel][1] = newY
